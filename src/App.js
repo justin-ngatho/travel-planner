d05b207bb75feb8b7dd75a304e5f2da40f3a366c
justin-ngatho <93423017+justin-ngatho@users.noreply.github.com>
2024-01-02 06:18:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      
+    </div>
+  );
+}
+
+export default App;
